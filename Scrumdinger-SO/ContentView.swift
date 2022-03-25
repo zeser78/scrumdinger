7651a0f93417c62ed76a6a7a05d0d9443cde6563
Sergio <solivares@xpaso.com>
2022-03-25 15:10:43 -0400

    Initial Commit

---
+++ Scrumdinger-SO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumdinger-SO
+//
+//  Created by Sergio Olivares on 3/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrumdinger-SO/Scrumdinger_SOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_SOApp.swift
+//  Scrumdinger-SO
+//
+//  Created by Sergio Olivares on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_SOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
